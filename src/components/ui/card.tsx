2163f63bdd98dff2c0853a1963dfc51c9796b585
Eduardo Campos <eduardo.campos.lhamas@gmail.com>
2025-11-05 13:55:21 +0000

    Eu queria dar um tapa no visual, mantendo o estilo mas melhorando ainda

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "border-2 border-primary/30 bg-black/50 p-1",
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6 border-b-2 border-primary/30", className)}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props}
   />
 ))
